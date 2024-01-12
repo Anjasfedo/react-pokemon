fe48855f59727cf6e61f459f248167bccadf8f10
AnjasFedo <fedoaf98@gmail.com>
2024-01-12 07:11:07 +0700

    Fetch Data with Error & Loading Handler

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -1,16 +1,14 @@
-import { useState, useEffect, use } from "react";
+import { use } from "react";
 
-const fetchData =async (url: string) => {
-    const res = await fetch(url)
+const fetchData = async (url: string) => {
+  const res = await fetch(url);
 
-    return res.json
-}
+  return res.json();
+};
 
 export const PokemonGrid = () => {
-    const url = 'https://jsonplaceholder.typicode.com/todos/1'
-    const data = use(fetchData(url))
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const data = use(fetchData(url));
 
-  return (
-    <div>{data.title}</div>
-  )
-}
+  return <div>{data.title}</div>;
+};
